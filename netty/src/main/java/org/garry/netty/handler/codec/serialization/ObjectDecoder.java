123ac4a024236449c5e022c2a9a3b1c1d47db6b8
garrychen <chenymj23@gmail.com>
2021-01-26 23:20:05 +0800

    ObjectDecoder init

--- netty/src/main/java/org/garry/netty/handler/codec/serialization/ObjectDecoder.java
+++ netty/src/main/java/org/garry/netty/handler/codec/serialization/ObjectDecoder.java
@@ -5,17 +5,59 @@ import org.garry.netty.channel.Channel;
 import org.garry.netty.channel.ChannelHandlerContext;
 import org.garry.netty.handler.codec.frame.FrameDecoder;
 
+import java.io.StreamCorruptedException;
+
 public class ObjectDecoder extends FrameDecoder {
 
+    private final int maxObjectSize;
+    private final ClassLoader classLoader;
+
+    public ObjectDecoder() {
+        this(1048576);
+    }
+
+    public ObjectDecoder(int maxObjectSize) {
+        this(maxObjectSize,Thread.currentThread().getContextClassLoader());
+    }
+
+    public ObjectDecoder(int maxObjectSize, ClassLoader classLoader) {
+        if(maxObjectSize <= 0)
+        {
+            throw new IllegalAccessException("maxObjectSize: " + maxObjectSize);
+        }
+        if(classLoader == null)
+        {
+            classLoader = Thread.currentThread().getContextClassLoader();
+        }
+        this.maxObjectSize = maxObjectSize;
+        this.classLoader = classLoader;
+    }
+
     @Override
-    protected Object decode(ChannelHandlerContext ctx, Channel channel, ChannelBuffer buffer) {
+    protected Object decode(ChannelHandlerContext ctx, Channel channel, ChannelBuffer buffer) throws Exception {
         if(buffer.readableBytes() < 4)
         {
             return null;
         }
         // todo why getInt() ???
         int dataLen = buffer.getInt(buffer.readerIndex());
+        if(dataLen <= 0)
+        {
+            throw new StreamCorruptedException("invalid data length: " + dataLen);
+        }
+        if(dataLen > maxObjectSize)
+        {
+            throw new StreamCorruptedException(
+                    "data length too bif: " +dataLen+ " (max: " + maxObjectSize + ')');
+        }
 
-        return null;
+        if(buffer.readableBytes() < dataLen + 4)
+        {
+            return null;
+        }
+
+
+
+        return ;
     }
 }
